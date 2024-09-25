89c38ab1fee47586bee8730b69c7ab69983bc2a2
Rudys Acosta <ncrousset926@gmail.com>
2024-09-25 00:29:09 -0400

    MergeSortedArray

--- main.go
+++ main.go
@@ -10,7 +10,9 @@ func main() {
 
 	// test := array.PlusOne([]int{9, 9, 9})
 
-	test := array.TwoSum([]int{3, 2, 4, 4}, 8)
+	// test := array.TwoSum([]int{3, 2, 4, 4}, 8)
+
+	test := array.MergeSortedArray([]int{2, 0}, 1, []int{1}, 1)
 
 	fmt.Println(test)
 }
